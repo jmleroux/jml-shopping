cf6221cf5d60cc2c091dc8b383ed9f5ad92808e9
jmleroux <jmleroux.pro@gmail.com>
2016-03-20 21:12:31 +0100

    refactor : namespace Core

--- spec/Jmleroux/Core/CategoryServiceSpec.php
+++ spec/Jmleroux/Core/CategoryServiceSpec.php
@@ -4,6 +4,7 @@ namespace spec\Jmleroux\Core;
 
 use Doctrine\DBAL\Connection;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class CategoryServiceSpec extends ObjectBehavior
 {
@@ -15,5 +16,17 @@ class CategoryServiceSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Jmleroux\Core\CategoryService');
+    }
+
+    function it_fetches_all_categories($connection)
+    {
+        $category = [
+            'id' => 1,
+            'label' => 'foo',
+        ];
+        $connection->fetchAll(Argument::type('string'))
+            ->willReturn([$category]);
+
+        $this->getAll()->shouldReturn([$category]);
     }
 }
