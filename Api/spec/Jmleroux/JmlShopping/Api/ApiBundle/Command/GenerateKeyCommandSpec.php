7684a0f6da0a4eb6f4b8737459dd0e7166ab62c9
jmleroux <jmleroux.pro@gmail.com>
2016-12-21 00:52:43 +0100

    PhpSpec

--- Api/spec/Jmleroux/JmlShopping/Api/ApiBundle/Command/GenerateKeyCommandSpec.php
+++ Api/spec/Jmleroux/JmlShopping/Api/ApiBundle/Command/GenerateKeyCommandSpec.php
@@ -2,16 +2,21 @@
 
 namespace spec\Jmleroux\JmlShopping\Api\ApiBundle\Command;
 
-use Jmleroux\JmlShopping\Api\ApiBundle\Command\UserAddCommand;
+use Jmleroux\JmlShopping\Api\ApiBundle\Command\GenerateKeyCommand;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UserAddCommandSpec extends ObjectBehavior
+class GenerateKeyCommandSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType(UserAddCommand::class);
+        $this->shouldHaveType(GenerateKeyCommand::class);
         $this->shouldHaveType('Symfony\Component\Console\Command\Command');
+    }
+
+    function it_can_run(InputInterface $input, OutputInterface $output)
+    {
+        $this->run($input, $output)->shouldReturn(0);
     }
 }
