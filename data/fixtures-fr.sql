ba4569c6546047530dcf38bf99eb417579096aaf
jmleroux <jmleroux.pro@gmail.com>
2017-03-01 21:53:33 +0100

    Add fixtures fr

---
+++ data/fixtures-fr.sql
@@ -0,0 +1,12 @@
+BEGIN TRANSACTION;
+insert into categories ("id", "label") values ('1', 'Frais');
+insert into categories ("id", "label") values ('2', 'Scolaire');
+insert into categories ("id", "label") values ('3', 'Jeux / Jouets');
+insert into categories ("id", "label") values ('4', 'Droguerie');
+insert into categories ("id", "label") values ('5', 'Condiments');
+insert into categories ("id", "label") values ('6', 'Boissons');
+insert into categories ("id", "label") values ('7', 'Bricolage');
+insert into categories ("id", "label") values ('8', 'Cuisine');
+insert into categories ("id", "label") values ('9', 'Légumes');
+insert into categories ("id", "label") values ('10', 'Surgelés');
+COMMIT;
