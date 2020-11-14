2a7b33dd2c85b16b8494d617393529048cb38ac4
jmleroux <jmleroux.pro@gmail.com>
2020-11-14 15:34:54 +0100

    Autofocus on product form

--- src/js/components/ProductForm.js
+++ src/js/components/ProductForm.js
@@ -1,14 +1,24 @@
-import React, {useContext} from "react";
+import React, {useContext, useRef} from "react";
 import {Button, Container, Dropdown, Form, Input} from 'semantic-ui-react'
 
 import store, {editProduct, updateProducts} from "../store";
 import apiProduct from "../apiProduct";
+
+const useFocus = () => {
+    const htmlElRef = useRef(null)
+    const setFocus = () => {
+        htmlElRef.current && htmlElRef.current.focus()
+    }
+
+    return [htmlElRef, setFocus]
+}
 
 const ProductForm = () => {
 
     const {state, dispatch} = useContext(store);
     const product = state.product;
     const categories = state.categories;
+    const [inputRef, setInputFocus] = useFocus();
 
     const handleSubmit = async (event) => {
         if (event) event.preventDefault();
@@ -16,6 +26,7 @@ const ProductForm = () => {
         const newProduct = result.data;
         dispatch(editProduct(newProduct));
         dispatch(updateProducts());
+        setInputFocus();
     };
 
     const handleChange = (event) => {
@@ -46,11 +57,12 @@ const ProductForm = () => {
             <Form onSubmit={handleSubmit}>
                 <div className='inline fields'>
                     <Form.Field>
-                        <Input type='text' name='name' value={product.name} onChange={handleChange}/>
+                        <Input type='text' name='name' autoFocus value={product.name} onChange={handleChange}
+                               ref={inputRef}/>
                     </Form.Field>
                     <Form.Field>
                         <Dropdown selection onChange={handleChangeCategory}
-
+                                  search
                                   options={categoryOptions}
                                   value={product.category_id}/>
                     </Form.Field>
