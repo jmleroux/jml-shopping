985a83b88707c250dc57b7aacfdcbf6e9bfd4b01
jmleroux <jmleroux.pro@gmail.com>
2021-11-14 12:44:02 +0100

    Remove useless stuff

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,7 +21,7 @@
         class="primary lighten-2 py-4 text-center white--text"
         cols="12"
       >
-        {{ new Date().getFullYear() }} — <strong>JML Shopping</strong>
+        ©2021 — <strong>JML Shopping</strong>
       </v-col>
     </v-row>
   </v-footer>
